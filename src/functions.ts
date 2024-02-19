53e7a4e34241393c313c7cce929a10f08583034a
Lucas Connors <lucas.revolutiontech@gmail.com>
2024-02-19 00:24:33 -0800

    feat: add support for additional functions

--- src/functions.ts
+++ src/functions.ts
@@ -1,8 +1,15 @@
 import every from "lodash/every";
+import max from "lodash/max";
+import maxBy from "lodash/maxBy";
+import min from "lodash/min";
+import minBy from "lodash/minBy";
 import some from "lodash/some";
+import lodashUnion from "lodash/union";
 
+const isnull = (x: any) => x == null;
 const or = (...boolExps: boolean[]) => some(boolExps);
 const and = (...boolExps: boolean[]) => every(boolExps);
+const union = (lists: unknown[][]) => lodashUnion(...lists);
 const count = (list: unknown[]) => list.length;
 const filter = (list: unknown[], lambda: (x: unknown) => any) =>
   list.filter(lambda);
@@ -10,9 +17,17 @@ const map = (list: unknown[], lambda: (x: unknown) => unknown) =>
   list.map(lambda);
 
 export const FUNCTIONS = {
+  isnull,
   or,
+  any: some,
   and,
+  all: every,
+  union,
   count,
+  min,
+  minby: minBy,
+  max,
+  maxby: maxBy,
   filter,
   map,
 };
---
+++ test/functions.test.ts
@@ -0,0 +1,75 @@
+import { FUNCTIONS } from "../src/functions";
+
+const { isnull, or, and, union, count, filter, map } = FUNCTIONS;
+
+describe("ISNULL", () => {
+  it("returns true for null values", () => {
+    expect(isnull(null)).toBe(true);
+  });
+
+  it("returns false for non-null falsey values", () => {
+    expect(isnull(false)).toBe(false);
+    expect(isnull(0)).toBe(false);
+  });
+
+  it("returns false for truthy values", () => {
+    expect(isnull(true)).toBe(false);
+    expect(isnull(2)).toBe(false);
+  });
+});
+
+describe("OR", () => {
+  it("returns false when all values are false", () => {
+    expect(or(false, false, false)).toBe(false);
+  });
+
+  it("returns true when any value is true", () => {
+    expect(or(true, false, false)).toBe(true);
+    expect(or(false, true, false)).toBe(true);
+  });
+});
+
+describe("AND", () => {
+  it("returns true when all values are true", () => {
+    expect(and(true, true, true)).toBe(true);
+  });
+
+  it("returns false when any value is false", () => {
+    expect(and(false, true, true)).toBe(false);
+    expect(and(true, false, true)).toBe(false);
+  });
+});
+
+describe("UNION", () => {
+  it("returns a combined array", () => {
+    expect(union([["foo", "bar"], ["baz"]])).toEqual(["foo", "bar", "baz"]);
+    expect(union([[], [], []])).toEqual([]);
+  });
+
+  it("supports an empty array", () => {
+    expect(union([])).toEqual([]);
+  });
+});
+
+describe("COUNT", () => {
+  it("returns the number of items in the array", () => {
+    expect(count(["foo"])).toBe(1);
+    expect(count(["foo", "bar", "baz"])).toBe(3);
+  });
+
+  it("supports empty arrays", () => {
+    expect(count([])).toBe(0);
+  });
+});
+
+describe("FILTER", () => {
+  it("filters the array to each item in the array that returns true for the given function", () => {
+    expect(filter([1, 2, 3], (x: number) => x % 2 == 1)).toEqual([1, 3]);
+  });
+});
+
+describe("MAP", () => {
+  it("transforms each item in the array by applying the given function", () => {
+    expect(map([1, 2, 3], (x: number) => x * 2)).toEqual([2, 4, 6]);
+  });
+});
